5d32dbc8046368fc4b4ab78a80e8a9341c4302c3
jadelcruz1 <jardelcruz6@gmail.com>
2023-10-26 21:12:27 -0300

    service

---
+++ src/main/java/br/com/jardel/todolist/service/TodoService.java
@@ -0,0 +1,44 @@
+package br.com.jardel.todolist.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import br.com.jardel.todolist.entity.Todo;
+import br.com.jardel.todolist.repository.TodoRepository;
+
+@Service
+public class TodoService {
+    private TodoRepository todorepository;
+
+    public TodoService(TodoRepository todorepository) {
+        this.todorepository = todorepository;
+    }
+
+    public List<Todo> create(Todo todo){
+        todorepository.save(todo);
+        return list();
+
+    }
+
+    public List<Todo> list(){
+        // ordenação da lista de tarefa primeiro por prioridade de forma descedente e depois por nome.
+       Sort sort =  Sort.by("prioridade").descending().and( Sort.by("nome").ascending()); 
+       return todorepository.findAll(sort);
+        
+    }
+
+    public List<Todo> update( Todo todo){
+        todorepository.save(todo);
+        return list();
+        
+    }
+
+    public List<Todo> delete( Long id){
+        todorepository.deleteById(id);
+        return list();
+        
+    }
+    
+}
