711075da048423f62d18f994d82602069b7fbb9b
jadelcruz1 <jardelcruz6@gmail.com>
2023-11-02 11:40:48 -0300

    cenarioTeste

--- src/main/java/br/com/jardel/todolist/controller/TodoController.java
+++ src/main/java/br/com/jardel/todolist/controller/TodoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jardel.todolist.entity.Todo;
 import br.com.jardel.todolist.service.TodoService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/todos")
@@ -24,7 +25,7 @@ public class TodoController {
     }
 
     @PostMapping
-    List <Todo> create(@RequestBody Todo todo){
+    List <Todo> create(@RequestBody @Valid Todo todo){
         return todoService.create(todo);
 
     }
